9bbef7e43359e36182d107a9ab4a6e06703777fc
Simo Arajarvi <simo.arajarvi@gmail.com>
2024-02-09 10:09:03 -0800

    Start navbar

--- gencx_dev/src/App.tsx
+++ gencx_dev/src/App.tsx
@@ -1,25 +1,34 @@
 import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  
-  return <Grid templateAreas={{
-            base: `"nav" "main" "bottom"`,
-            lg: `"nav nav" "aside main" "bottom bottom"`,
-          }}
-          templateColumns={{
-            base: '1fr',
-            lg: '250px 1fr'
-          }}>
-
-          <GridItem area = 'nav' bg='blue'>Nav</GridItem>
-          <Show above="lg">
-          <GridItem area = 'aside' bg='orange'>Aside</GridItem>
-          </Show>
-          <GridItem area = 'main' bg='green'>Main</GridItem>
-          <GridItem area = 'bottom' bg='grey'>Bottom</GridItem>
-          
-           </Grid>
-
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main" "bottom"`,
+        lg: `"nav nav" "aside main" "bottom bottom"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr",
+      }}
+    >
+      <GridItem area="nav" bg="blue">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="orange">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="green">
+        Main
+      </GridItem>
+      <GridItem area="bottom" bg="grey">
+        Bottom
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
---
+++ gencx_dev/src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+import { HStack, Text } from "@chakra-ui/react";
+
+const NavBar = () => {
+  return <Text>Nabbar...</Text>
+}
+
+export default NavBar;
