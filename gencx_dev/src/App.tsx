217bf1a1cd289222f2b750d01703ebc4fb48341c
Simo Arajarvi <simo.arajarvi@gmail.com>
2024-02-09 10:00:33 -0800

    Add grids

--- gencx_dev/src/App.tsx
+++ gencx_dev/src/App.tsx
@@ -1,8 +1,24 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Box, Flex, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 
 function App() {
   
-  return <Button colorScheme='blue'>Button</Button>;
+  return <Grid templateAreas={{
+            base: `"nav" "main" "bottom"`,
+            lg: `"nav nav" "aside main" "bottom bottom"`,
+          }}
+          templateColumns={{
+            base: '1fr',
+            lg: '250px 1fr'
+          }}>
+
+          <GridItem area = 'nav' bg='blue'>Nav</GridItem>
+          <Show above="lg">
+          <GridItem area = 'aside' bg='orange'>Aside</GridItem>
+          </Show>
+          <GridItem area = 'main' bg='green'>Main</GridItem>
+          <GridItem area = 'bottom' bg='grey'>Bottom</GridItem>
+          
+           </Grid>
 
 }
 
